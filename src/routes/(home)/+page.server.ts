b0764b734fc05c14c4dd86075b384581f3821cb2
Satria <satr14@homeserver.local>
2024-07-15 19:04:27 +0700

    payload too big fix

--- src/routes/(home)/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { getContainers } from '$lib';
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-    return { sections: await getContainers() };
-}) satisfies PageServerLoad;
