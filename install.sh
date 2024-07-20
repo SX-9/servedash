fbb75bf05b36ee1478daddf566e0b25c1389d56e
Satria <satr14@homeserver.local>
2024-07-20 11:50:40 +0700

    tweaks and fixes

--- install.sh
+++ install.sh
@@ -1,46 +1,69 @@
 #! /bin/bash
 
 set -e
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root" >&2
-  exit 1
-fi
 
-INSTALLATION_PATH="/opt/servedash"
-CONFIGURATION_PATH="/etc/servedash"
+check_root() {
+  if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root" >&2
+    exit 1
+  fi
+}
 
-echo "Installing ServeDash files..."
-[ ! -d "./build" ] && npx vite build
-[ ! -d "/opt/servedash" ] && mkdir -p $INSTALLATION_PATH
-rm -rf $INSTALLATION_PATH/*
-cp -r ./package.json ./package-lock.json ./node_modules ./build/* $INSTALLATION_PATH
+install_servedash_files() {
+  echo "Installing ServeDash files..."
+  [ ! -d "./build" ] && npx vite build
+  [ ! -d "/opt/servedash" ] && mkdir -p $INSTALLATION_PATH
+  rm -rf $INSTALLATION_PATH/*
+  cp -r ./package.json ./package-lock.json ./node_modules ./build/* $INSTALLATION_PATH
+}
 
-echo "Installing ServeDash services..."
-sed -i "s|{{ installation_config_path }}|$CONFIGURATION_PATH|g" ./systemd/servedash.service
-cp ./systemd/* /etc/systemd/system
+install_servedash_services() {
+  echo "Installing ServeDash services..."
+  cp ./systemd/* /etc/systemd/system
+}
 
-echo "Setting up Nginx..."
-[ -f "/etc/nginx/nginx.conf" ] && mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
-cp ./nginx.conf /etc/nginx/nginx.conf
+setup_nginx() {
+  echo "Setting up Nginx..."
+  [ -f "/etc/nginx/nginx.conf" ] && mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+  cp ./nginx.conf /etc/nginx/nginx.conf
+}
 
-echo "Creating configuration directory..."
-[ ! -d "$CONFIGURATION_PATH" ] && mkdir -p $CONFIGURATION_PATH
-if [[ ! -f "$CONFIGURATION_PATH/htpasswd" ]]; then
-  echo -n "Enter a username: "
-  read username
-  htpasswd -c $CONFIGURATION_PATH/htpasswd $username
-fi
-cp -nf ./config/* $CONFIGURATION_PATH
-chmod 666 $CONFIGURATION_PATH/config.yaml $CONFIGURATION_PATH/theme.yaml
-chmod 644 $CONFIGURATION_PATH/htpasswd
+create_configuration_directory() {
+  echo "Creating configuration directory..."
+  [ ! -d "$CONFIGURATION_PATH" ] && mkdir -p $CONFIGURATION_PATH
+  if [[ ! -f "$CONFIGURATION_PATH/htpasswd" ]]; then
+    echo -n "Enter a username: "
+    read username
+    htpasswd -c $CONFIGURATION_PATH/htpasswd $username
+  fi
+  cp -n ./config/* $CONFIGURATION_PATH
+  chmod 666 $CONFIGURATION_PATH/config.yaml $CONFIGURATION_PATH/theme.yaml
+  chmod 644 $CONFIGURATION_PATH/htpasswd
+}
 
-echo "Starting ServeDash services..."
-systemctl daemon-reload
-systemctl enable --now servedash.socket nginx
-systemctl restart nginx
+start_servedash_services() {
+  echo "Starting ServeDash services..."
+  systemctl daemon-reload
+  systemctl enable --now servedash.socket nginx
+  systemctl restart nginx
+}
 
-echo "Installation Complete!"
-echo "Files: /opt/servedash"
-echo "Service: servedash.socket"
-echo "Dashboard (nginx proxy): http://localhost:8197"
-echo "Login with your username and the password you set"
+main() {
+  INSTALLATION_PATH="/opt/servedash"
+  CONFIGURATION_PATH="/etc/servedash"
+
+  check_root
+  install_servedash_files
+  install_servedash_services
+  setup_nginx
+  create_configuration_directory
+  start_servedash_services
+
+  echo "Installation Complete!"
+  echo "Files: /opt/servedash"
+  echo "Service: servedash.socket"
+  echo "Dashboard (nginx proxy): http://localhost:8197"
+  echo "Login with your username and the password you set"
+}
+
+main
